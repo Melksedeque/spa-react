c962fc8198f6718131a66d891de19bc8788621ee
Melksedeque <freelancer@melksedeque.com.br>
2025-02-03 14:17:53 -0300

    Criação de Página de contato

---
+++ src/paginas/Contato/index.js
@@ -0,0 +1,11 @@
+import "./Contato.css";
+
+export default function Contato() {
+  return (
+    <div className="contato">
+      <h1>Contato</h1>
+      <p>Telefone: (11) 97691-7114</p>
+      <p>Email: freelancer@melksedeque.com.br</p>
+    </div>
+  );
+}
