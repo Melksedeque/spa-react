59cbbfb721a5d61fdbc3671d09c93187c4d89a51
Melksedeque <freelancer@melksedeque.com.br>
2025-02-03 13:15:49 -0300

    Criação de componentes de páginas

---
+++ src/paginas/Inicio/index.js
@@ -0,0 +1,9 @@
+import "./Inicio.css";
+
+export default function Inicio() {
+  return (
+    <div className="Inicio">
+      <h1>Inicio</h1>
+    </div>
+  );
+}
---
+++ src/paginas/SobreMim/index.js
@@ -0,0 +1,9 @@
+import "./SobreMim.css";
+
+export default function SobreMim() {
+  return (
+    <div className="SobreMim">
+      <h1>Sobre Mim</h1>
+    </div>
+  );
+}
