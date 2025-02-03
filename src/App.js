1f659c647e00b8ad0627f9cb9a0ac06c35770744
Melksedeque <freelancer@melksedeque.com.br>
2025-02-03 13:37:59 -0300

    Troca de rotas do JS Vanilla para usar o ReactRouterDOM

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import Inicio from "./paginas/Inicio";
-import SobreMim from "./paginas/SobreMim";
 
 function App() {
-  return (
-    <BrowserRouter>
-    </BrowserRouter>
-  );
+  return <BrowserRouter></BrowserRouter>;
 }
 
 export default App;
