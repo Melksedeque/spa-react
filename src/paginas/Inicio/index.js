9f5c13018659f83b3134f8b8c21c9146dacc92a7
Melksedeque <freelancer@melksedeque.com.br>
2025-02-03 16:46:12 -0300

    Criação de banner na página inicial; CSS dos posts que irão aparecer na home

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,9 +1,11 @@
+import Banner from "componentes/Banner";
 import styles from "./Inicio.module.css";
 
 export default function Inicio() {
   return (
-    <div className={styles.home}>
+    <main className={styles.home}>
+      <Banner />
       <h1>Inicio</h1>
-    </div>
+    </main>
   );
 }
