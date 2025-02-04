cb75825189bf3316df1fc20127b468ebe162b301
Melksedeque <freelancer@melksedeque.com.br>
2025-02-04 17:54:51 -0300

    Componente JS de Pagina Padrão

---
+++ src/componentes/PaginaPadrao/index.js
@@ -0,0 +1,14 @@
+import Banner from "componentes/Banner";
+import styles from "./PaginaPadrao.module.css";
+import { Outlet } from "react-router-dom";
+
+export default function PaginaPadrao({ children }) {
+  return (
+    <main className={styles.home}>
+      <Banner />
+      <div className={styles["pagina-padrao"]}>
+        <Outlet />
+      </div>
+    </main>
+  );
+}
