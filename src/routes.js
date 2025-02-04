9fbccd42e8016553d7a3fc67b2e9544c923f9d15
Melksedeque <freelancer@melksedeque.com.br>
2025-02-04 17:58:48 -0300

    Alteração de rota para executar o elemento PaginaPadrao em rotas especificas

--- src/routes.js
+++ src/routes.js
@@ -5,15 +5,18 @@ import SobreMim from "./paginas/SobreMim";
 import Contato from "./paginas/Contato";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
-        <Route path="/contato" element={<Contato />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+          <Route path="/contato" element={<Contato />} />
+        </Route>
         <Route path="*" element={<h1>Página não encontrada</h1>} />
       </Routes>
       <Rodape />
