c6f84b5ebdae7d1e2cc7bc27bc8dddc59997b870
Melksedeque <freelancer@melksedeque.com.br>
2025-02-05 19:21:19 -0300

    Mudança na chamada do componente Post para PostCards

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,13 +1,13 @@
 import styles from "./Inicio.module.css";
 import posts from "json/posts.json";
-import Post from "componentes/Post";
+import PostCards from "componentes/PostCards";
 
 export default function Inicio() {
   return (
     <ul className={styles.posts}>
       {posts.map((post) => (
         <li key={post.id}>
-          <Post post={post} />
+          <PostCards post={post} />
         </li>
       ))}
     </ul>
