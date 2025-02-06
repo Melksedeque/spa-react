5cdaa32504ab2ce88db5f9a345da17e7d8a6277f
Melksedeque <freelancer@melksedeque.com.br>
2025-02-05 23:21:06 -0300

    Estrutura das páginas de post

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,12 +1,25 @@
 import { useParams } from "react-router-dom";
-import styles from "./Post.module.css";
+import "./Post.css";
+import posts from "json/posts.json";
+import PostModelo from "componentes/PostModelo";
+import ReactMarkdown from "react-markdown";
 
-export default function Post({ post }) {
+export default function Post() {
   const params = useParams();
-  console.log(params);
+  const post = posts.find((post) => {
+    return post.id === Number(params.id);
+  });
+
   return (
-    <div className={styles.post}>
-      <h2>Post com ID: {params.id}</h2>
+    <div className="post">
+      <PostModelo
+        fotoCapa={`/assets/posts/${post.id}/capa.png`}
+        titulo={post.titulo}
+      >
+        <div className="post-markdown-container">
+          <ReactMarkdown>{post.texto}</ReactMarkdown>
+        </div>
+      </PostModelo>
     </div>
   );
 }
