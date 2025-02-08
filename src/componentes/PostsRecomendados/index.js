44f222a2c65fd05056f01fb92dd7c244a11c23d9
Melksedeque <freelancer@melksedeque.com.br>
2025-02-08 16:24:03 -0300

    Passando props de posts recomendados

--- src/componentes/PostsRecomendados/index.js
+++ src/componentes/PostsRecomendados/index.js
@@ -1,12 +1,12 @@
 import styles from "./PostsRecomendados.module.css";
 import Inicio from "paginas/Inicio";
 
-export default function PostsRecomendados() {
+export default function PostsRecomendados({ estePost }) {
   return (
     <div className={styles.PostsRecomendadosContainer}>
       <h2 className={styles.tituloSecao}>Outros posts que você pode gostar:</h2>
       <div className={styles.PostsRecomendados}>
-        <Inicio />
+        <Inicio postsRecomendados={true} limite={4} exceto={estePost} />
       </div>
     </div>
   );
