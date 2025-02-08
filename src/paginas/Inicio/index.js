7d6c322c53b6b8d5ac994299e6a28bb96a3fee17
Melksedeque <freelancer@melksedeque.com.br>
2025-02-08 16:55:36 -0300

    Chamada de props de limite e exceção; Uso de useLocation para verificar se é página de post; condicional de exibição de posts recomendados em caso de página de post;

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,11 +1,27 @@
 import styles from "./Inicio.module.css";
 import posts from "json/posts.json";
 import PostCards from "componentes/PostCards";
+import { useLocation } from "react-router-dom";
 
-export default function Inicio() {
+export default function Inicio({ limite = posts.length, exceto }) {
+  const local = useLocation();
+  const paginaDePost = local.pathname.includes("post");
+
+  const postsParaExibir = paginaDePost
+    ? posts.filter((post) => post.id !== exceto).slice(0, limite)
+    : posts;
+
+  if (!postsParaExibir.length) {
+    return (
+      <section className={styles.semPosts}>
+        <p>Não há posts para exibir</p>
+      </section>
+    );
+  }
+
   return (
     <ul className={styles.posts}>
-      {posts.map((post) => (
+      {postsParaExibir.map((post) => (
         <li key={post.id}>
           <PostCards post={post} />
         </li>
